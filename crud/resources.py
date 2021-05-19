ea1b3def1b133f9477db45bc1631ea399171468f
caesarclown9 <caesarclown9@gmail.com>
2021-05-19 20:50:26 +0600

    added and filled new templates

--- crud/resources.py
+++ crud/resources.py
@@ -5,6 +5,6 @@ from .models import Tables
 class TablesResources(resources.ModelResource):
     class Meta:
         model = Tables
-        # fields = [field.name for field in Tables._meta.fields]
-        exclude = ['name']
-        # import_id_fields = ['num']
+        fields = ['num','params', 'norms', 'method', 'note']
+        # exclude = ('name', 'id')
+        import_id_fields = ['num']
